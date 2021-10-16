821f8dd4085923b16d6d2dbc48f9c0a23d225950
Rachana1104 <87826210+Rachana1104@users.noreply.github.com>
2021-10-16 23:20:51 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,24 @@
+
 import React from "react";
-import { Link } from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 
 const NavBar = () => {
-    return ( <
-        nav >
-
-        <
-        h3 >
-        News Report <
-        /h3> {
-            /* <ul className="nav-links">
-                            <Link to='/'>
-                            <li>Home</li>
-                            </Link>
-                        </ul> */
-        } <
-        /nav>
+    return (
+        <nav>
+            
+            <h3>
+                News Report
+            </h3>
+            {/* <ul className="nav-links">
+                <Link to='/'>
+                <li>Home</li>
+                </Link>
+            </ul> */}
+        </nav>
     );
 }
 
 
 export default NavBar
+
+
