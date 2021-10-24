13a47a2c7c8a7c16842c5491acda395a9c32105a
Rachana1104 <y.rachana1104@gmail.com>
2021-10-24 19:21:58 +0530

    css modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import NewsApi from './components/newsApi';
 import NavBar from './components/NavBar';
 import NewsDetails from './components/NewsDetails';
-
 import './App.css';
 import {
     BrowserRouter as Router,
--- src/components/NewsDetails.js
+++ src/components/NewsDetails.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
-const NewsDetails = ({card,setCard}) => {
-    return (
-        <div className="newsdetails">
-            <h2 className="heading">{card.title}</h2>
-            <img className="image" src={card.urlToImage} alt=""/>
-            <div><h4 className="description" >{card.description}</h4></div>
-            <div><h4 className="content" >{card.content}</h4></div>
-            <h4>Author: {card.author}</h4>
-        </div>
+const NewsDetails = ({ card, setCard }) => {
+    return ( <
+        div className = "newsdetails" >
+        <
+        h2 className = "heading" > { card.title } < /h2> <
+        img className = "image"
+        src = { card.urlToImage }
+        alt = "" / >
+        <
+        div > < h4 className = "description" > { card.description } < /h4></div >
+        <
+        div > < h4 className = "content" > { card.content } < /h4></div >
+        <
+        h4 > Author: { card.author } < /h4> <
+        /div>
     )
 }
 
